f1025e111c3d5ba17484fd5b922931f8a89ee70c
Shovon <shvnry11@gmail.com>
2017-08-04 11:01:09 +0530

    Resolved file conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< 06d50b2ec7d819782d58d6a74494ec6c3abb3ae4
   get 'welcome/index'
 
   root 'welcome#index'
@@ -7,9 +6,5 @@ Rails.application.routes.draw do
   resources :items, except: :destroy
   resources :categories
   resources :brands
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-=======
   resources :users
->>>>>>> Made skeleton of user manipulation in the system
 end
