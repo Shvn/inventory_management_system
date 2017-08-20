74f815345daf3a26859839acb0344cc9df48bd3a
Shovon <shvnry11@gmail.com>
2017-08-20 14:38:05 +0530

    Updated fixes

--- app/helpers/allotments_helper.rb
+++ app/helpers/allotments_helper.rb
@@ -10,12 +10,4 @@ module AllotmentsHelper
   def check_quantity
     notify_admins if @item.buffer_quantity_reached?
   end
-
-  def notify_admins
-    @admins = User.type_admin
-    @priority = @item.calculate_priority
-    @admins.each do |admin|
-      BufferMailer.low_quantity_email(admin, @item, @priority).deliver_now
-    end
-  end
 end
